4a62cb11f89e364a482deabe995759cd450fe24b
danifrakanza@gmail.com <danifrakanza@gmail.com>
2025-04-28 18:10:17 +0700

    - Menggunakan NavigationContainer dan createNativeStackNavigator dari @react-navigation/native dan @react-navigation/native-stack. Menambahkan tiga screen utama ke dalam navigator: Beranda, Detail Cuaca, Tentang

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import jadian from'./app/screens/jadian';
+import DetailCuacaScreen from'./app/screens/DetailCuacaScreen';
+import TentangScreen from './app/screens/TentangScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Beranda">
+        <Stack.Screen name="Beranda" component={jadian} />
+        <Stack.Screen name="Detail Cuaca" component={DetailCuacaScreen} />
+        <Stack.Screen name="Tentang" component={TentangScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
